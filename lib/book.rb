84ed369a4b0c5228c9b8e977ad7f2d44ac6e0624
Tara Sabella <tarasabella@gmail.com>
2018-12-14 21:14:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
+  
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  
+  GENRES = [ ]
+  
+  #array that keeps track of all genres 
 
   def initialize(title)
     @title = title
@@ -9,5 +14,9 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+# create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
