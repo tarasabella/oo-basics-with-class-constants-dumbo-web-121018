18f9b9b516609a24a10931c4000e9eba085ed058
Tara Sabella <tarasabella@gmail.com>
2018-12-14 21:13:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,15 +6,12 @@ BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDs << brand unless BRANDS.include?(brand)
   end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-# create the writer for genre and add the logic for the class constant
-  def BRANDS=(brand)
-    @brand = brand
-    BRANDS << brand
-  end.unique
+
 end
